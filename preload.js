54a4120774776f78c88de6f2729c359a42dabe7c
欧小川 <Aouchinx@88.com>
2022-05-24 13:15:28 +0800

    - add log - improve query string handling

--- preload.js
+++ preload.js
@@ -5,6 +5,8 @@ const API_KEY = '0f2b77f461b2884086fccc2ac6358c9f'
 
 const searchMovie = ({ query, year }) => {
   const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=zh&query=${encodeURIComponent(query)}&year=${year}`
+  console.log(query, year)
+  console.log(url)
   return axios.get(url)
     .then(({ status, data }) => {
       if (status === 200 && !!data?.results?.length) {
@@ -27,23 +29,28 @@ window.exports = {
       search: (action, text, callback) => {
         if (!text) {
           callback([])
+          console.log('text is empty')
           return
         }
 
         text.match(/[\w().-]+\.(\d{4})\..*/)
-        const match = text.match(/^(..+) (\d{4})$/) ||
-          text.match(/^(..+) \((\d{4})\)$/) ||
-          text.match(/Top\d{3}\.([^\x00-\xff]+)\.([\w().]+)\.(\d{4})\..*/) ||
+        const match = text.match(/Top\d{3}\.([\u4e00-\u9fa5()]+)\.([\w().]+)\.(\d{4})\..*/) ||
+          text.match(/^(..+)\s(\d{4})$/) ||
+          text.match(/^(..+)\s\((\d{4})\)$/) ||
           text.match(/([\w().-]+)\.(\d{4})\..*/) ||
           text.match(/([\w().\s-]+)\s(\d{4})\s.*/)
 
-        if (!!match) {
-          const [ query, year ] = match.slice(match.length - 2)
-          window.searchMovie({
-            query: query.replace(/[.\-]/g, ' ').trim(),
-            year: year.trim()
-          }).then(callback)
+        if (!match) {
+          callback([])
+          console.log('text not match')
+          return
         }
+
+        const [ query, year ] = match.slice(match.length - 2)
+        window.searchMovie({
+          query: query.replace(/[.\-]/g, ' ').replace('(导演剪辑版)','').replace('aka The Professional Directors Cut').trim(),
+          year: year.trim()
+        }).then(callback)
       },
       select: (action, itemData) => {
         utools.copyText(itemData.title)
